b8e86eb8f2f4527d0a1be89a6981015e9aea59b7
abishek982001 <abishek982001@gmail.com>
2020-07-01 21:34:42 +0530

    Completed Face Recognition and also added code for name entry in csv

--- Face Recognition.py
+++ Face Recognition.py
@@ -2,6 +2,7 @@ import cv2
 import face_recognition
 import os
 import numpy as np
+from datetime import datetime
 
 path = 'Images For Attendance'
 images=[]
@@ -22,6 +23,18 @@ def findEncodings(images):
         encode = face_recognition.face_encodings(img)[0]
         encodeList.append(encode)
     return encodeList
+
+def enter_name(name):
+    with open('Entry.csv', 'r+') as f:
+        myDataList = f.readlines()
+        nameList=[]
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%H:%M:%S')
+            f.writelines('\n{},{}'.format(name, dtString))
 
 encodeList = findEncodings(images)  
 print("Encoding Complete")
@@ -46,10 +59,11 @@ while True:
             name = classNames[matchIndex].upper()
             print(name)
             y1,x2,y2,x1 = faceLoc
-            y1,x2,y2,x1 = y1*4,x2*4,y2*4,x1*4
+            y1,x2,y2,x1 = y1*4,x2*4,y2*4,x1*4  # To revert the earlier resize 
             cv2.rectangle(img, (x1,y1), (x2,y2), (0,255,0), 2)
             cv2.rectangle(img, (x1,y2-35), (x2,y2), (0,255,0), cv2.FILLED)
             cv2.putText(img, name, (x1+6,y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 2)
+            enter_name(name)
         
     cv2.imshow('Web Cam', img)
     cv2.waitKey(1)

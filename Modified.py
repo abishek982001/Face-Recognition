f8d64ff82f906c5c9dadf29ef1a84390dc1a29d0
abishek982001 <abishek982001@gmail.com>
2020-07-02 13:14:19 +0530

    Bug Fixes

--- Modified.py
+++ Modified.py
@@ -21,16 +21,17 @@ class FaceRecognition:
             self.images.append(curImg)
             self.classNames.append(os.path.splitext(name)[0])
         print(self.classNames)
-
+        
     def findEncodings(self,images):
         for img in self.images:
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             encode = face_recognition.face_encodings(img)[0]
             self.encodeList.append(encode)
-        self.encodeList = self.findEncodings(self.images)
-        print("Encoding Complete")
+        
 
     def initiateRecognition(self):
+        encodeList = self.findEncodings(self.images)  
+        print("Encoding Complete")
         cap = cv2.VideoCapture(0)
         while True:
             success, img = cap.read()
@@ -41,13 +42,13 @@ class FaceRecognition:
             encodeCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
     
             for encodeFace, faceLoc in zip(encodeCurFrame, facesCurFrame):
-                matches = face_recognition.compare_faces(encodeList, encodeFace)
-                faceDis = face_recognition.face_distance(encodeList, encodeFace)
+                matches = face_recognition.compare_faces(self.encodeList, encodeFace)
+                faceDis = face_recognition.face_distance(self.encodeList, encodeFace)
                 print(faceDis)
                 matchIndex = np.argmin(faceDis)
 
                 if matches[matchIndex]:
-                    name = classNames[matchIndex].upper()
+                    name = self.classNames[matchIndex].upper()
                     print(name)
                     y1,x2,y2,x1 = faceLoc
                     y1,x2,y2,x1 = y1*4,x2*4,y2*4,x1*4  # To revert the earlier resize 
@@ -55,11 +56,9 @@ class FaceRecognition:
                     cv2.rectangle(img, (x1,y2-35), (x2,y2), (0,255,0), cv2.FILLED)
                     cv2.putText(img, name, (x1+6,y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 2)
                     self.enter_name(name)
-        return img
 
-    def openWebCam(self,img):
-        cv2.imshow('Web Cam', img)
-        cv2.waitKey(1)
+            cv2.imshow('Web Cam', img)
+            cv2.waitKey(1)
 
     def enter_name(self,name):
         with open('Entry.csv', 'r+') as f:
@@ -67,7 +66,7 @@ class FaceRecognition:
             for line in myDataList:
                 entry = line.split(',')
                 self.nameList.append(entry[0])
-            if name not in nameList:
+            if name not in self.nameList:
                 now = datetime.now()
                 dtString = now.strftime('%H:%M:%S')
                 f.writelines('\n{},{}'.format(name, dtString))
@@ -75,3 +74,5 @@ class FaceRecognition:
 
 obj = FaceRecognition()
 obj.fileNames()
+obj.initiateRecognition()
+
